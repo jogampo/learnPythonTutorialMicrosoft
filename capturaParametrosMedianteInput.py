1ce8b98cd484df10e88d579350f4caced5d50884
jogampo <josegam@gmail.com>
2022-10-21 23:20:18 +0200

    Se incluye el captura parámetros

---
+++ capturaParametrosMedianteInput.py
@@ -0,0 +1,13 @@
+print("Welcome to the greeter program")
+name = input("Enter your name ")
+print("Greetings: " + name)
+
+print("calculator program: Sin Casting")
+first_number = input("first number: ")
+second_number = input("second number: ")
+print(first_number + second_number)
+
+print("calculator program: Con Casting")
+first_number = input("first number: ")
+second_number = input("second number: ")
+print(int(first_number) + int(second_number))
